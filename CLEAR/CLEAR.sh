bc8a4bd72d405a2fae55027d5c0d0d729a0eb584
mphillips67 <mphillips6789@gmail.com>
2019-11-12 20:34:51 -0800

    Add files via upload

---
+++ CLEAR/CLEAR.sh
@@ -0,0 +1,9 @@
+#commands used to run CLEAR on sync files with 4, 8, 13, or all 17 timepoints.
+
+python CLEAR.py --sync DS_4_tp.sync --out DS_4_clear_output.out
+
+python CLEAR.py --sync DS_8_tp.sync --out DS_8_clear_output.out
+
+python CLEAR.py --sync DS_13_tp.sync --out DS_13_clear_output.out
+
+python CLEAR.py --sync All_17_tp.sync --out Al_17_clear_output.out
